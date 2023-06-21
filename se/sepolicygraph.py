ef0cd48027ad562b23c7cf4d8de1d65dbaf2cda1
zhipengxu (aka. Ainevsia) <zhipengxu@sjtu.edu.cn>
2023-06-21 07:26:37 +0000

    seandroid learned a lot

--- se/sepolicygraph.py
+++ se/sepolicygraph.py
@@ -20,22 +20,24 @@ class SELinuxPolicyGraph(setools.SELinuxPolicy):
 
         # base identifiers
         classes: Dict[str, Dict[str, Union[List[str], str]]] = {}
-        attributes: Dict[str, List[str]] = {}   # key: attribute name, value: list of types
-        commons: Dict[str, List[str]] = {}
-        types: Dict[str, List[str]] = {}  
+        attributes: Dict[str, List[str]] = {}   # 所有拥有attribute的type
+        commons: Dict[str, List[str]] = {}      # 记录一个common的所有perms
+        types: Dict[str, List[str]] = {}        # 一个type的所有attribute，如果有的话；如果是alias，记录它的type
         aliases: Dict[str, bool] = {}
-        ta_used: Set[str] = set()
+        ta_used: Set[str] = set()               # 记录已经使用过的types和attributes
 
         # define type attributes
         Logger.debug(f'typeattributes len: {len(self.typeattributes())}')
         for attribute_ in cond_sort(self.typeattributes()):
             attributes[str(attribute_)] = []    # empty list
-        
+        print(len(attributes))
+        print([i for i in attributes][:10])
         # access vectors
         Logger.debug(f'commons len: {len(self.commons())}')
         for common_ in cond_sort(self.commons()):
             commons[str(common_)] = [str(x) for x in common_.perms]
-        
+        print(len(commons))
+        print(commons)
         # security object classes
         Logger.debug(f'classes len: {len(self.classes())}')
         for class_ in cond_sort(self.classes()):
@@ -46,57 +48,62 @@ class SELinuxPolicyGraph(setools.SELinuxPolicy):
                 parent = None
 
             perms: List[str] = [str(x) for x in class_.perms]
+            # class class_id [ inherits common_set ] [ { perm_set } ]
             classes[str(class_)] = { "perms" : perms, "parent" : parent }
             # print(classes[str(class_)])
-        
+        print(len(classes))
+        print([i for i in classes][:20])
+
+
         # define types, aliases and attributes
+        #                                        type type_id [alias alias_id] [, attribute_id];
         Logger.debug(f'types len: {len(self.types())}')
         for type_ in cond_sort(self.types()):
-            name: str = str(type_)
-            # print(type_)
+            name: str = str(type_)  # typename type_id
             for attr in type_.attributes():
-                # print(attr)
                 attributes[str(attr)] += [name]
-            # print('1111')
             for alias in type_.aliases():
-                # print(alias)
                 types[str(alias)] = name
                 aliases[str(alias)] = True
 
             types[name] = [str(x) for x in type_.attributes()]
-            # exit(1)
-        # exit(1)
+        print(len(types))
+        
         # define type enforcement rules
         for terule_ in cond_sort(self.terules()):
             # allowxperm rules
-            if isinstance(terule_, AVRuleXperm):
-                # "{0.ruletype} {0.source} {0.target}:{0.tclass} {0.xperm_type}"
-                ta_used.add(str(terule_.source))
-                ta_used.add(str(terule_.target))
-
             # allow/dontaudit/auditallow/neverallow rules
-            elif isinstance(terule_, AVRule):
+            if isinstance(terule_, AVRuleXperm) or isinstance(terule_, AVRule):
+                # "{0.ruletype} {0.source} {0.target}:{0.tclass} {0.xperm_type}"
                 # "{0.ruletype} {0.source} {0.target}:{0.tclass}"
                 ta_used.add(str(terule_.source))
                 ta_used.add(str(terule_.target))
+
             # type_* type enforcement rules
             elif isinstance(terule_, TERule):
+                # type_transition ITouchservice crash_dump_exec:process crash_dump;
                 # "{0.ruletype} {0.source} {0.target}:{0.tclass} {0.default}".format(terule_)
-                ta_used.add(str(terule_.source))
-                ta_used.add(str(terule_.target))
-                ta_used.add(str(terule_.default))
+                ta_used.add(str(terule_.source))    # ITouchservice
+                ta_used.add(str(terule_.target))    # crash_dump_exec
+                ta_used.add(str(terule_.default))   # crash_dump
             elif isinstance(terule_, FileNameTERule):
+                # type_transition zygote system_data_file:dir ctrl_socket_file ctrl_socket;
+                # type_transition system_server system_data_file:sock_file system_ndebug_socket ndebugsocket;
+                # type_transition system_server system_data_file:dir push_data_file hwpush_files;
+                # type_transition system_server system_data_file:dir data_system_iaware iaware;
+                # type_transition system_server system_data_file:dir app_acc_file app_acc;
+                # type_transition hal_wifi_supplicant_default wifi_data_file: dir   wpa_socket   sockets;
+                # type_transition source_type                 target_type   : class default_type object_name;
+                
                 ta_used.add(str(terule_.source))
                 ta_used.add(str(terule_.target))
                 ta_used.add(str(terule_.default))
+                ta_used.add(str(terule_.filename))  # filename type_transition rule
 
             else:
-                print(terule_)
-                print(type(terule_))
-
-                from IPython import embed; embed()
+                # this is unreachable: AnyTERule = Union["AVRule", "AVRuleXperm", "TERule", "FileNameTERule"]
                 raise RuntimeError("Unhandled TE rule")
-
+        
         print("--= Useless attributes list =--")
         for k in attributes.keys():
             if k not in ta_used:
@@ -115,3 +122,6 @@ class SELinuxPolicyGraph(setools.SELinuxPolicy):
 
         print("--= done =--")
         return
+
+    def build_graph(self):
+        pass
