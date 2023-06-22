21c62115ffcc486f5c69ab18ab62509ba4e06fa3
zhipengxu (aka. Ainevsia) <zhipengxu@sjtu.edu.cn>
2023-06-22 03:40:16 +0000

    apply_file_contexts

--- fs/filesysteminstance.py
+++ fs/filesysteminstance.py
@@ -1,6 +1,7 @@
 import re
 from typing import List, Tuple
 from android.init import AndroidInit
+from android.sepolicy import SELinuxContext
 from fs.filecontext import AndroidFileContext
 from se.sepolicygraph import PolicyGraph
 from utils.logger import Logger
@@ -49,27 +50,90 @@ class FileSystemInstance:
         pass
 
     def apply_file_contexts(self):
-        # TODO: need to refine this process according the real context initialization.
+        recovered_labels = 0
+        dropped_files: List[str] = []
+        # 遍历文件系统中的所有文件 file 是一个文件路径
         for file in self.init.asp.combined_fs.files:
-            matches: List[AndroidFileContext] = self.get_file_context_matches(file)
-            if len(matches) == 0 or file in self.init.asp.combined_fs.mount_points:
+            label_from_file_context: bool = True    # 假设能够从file_context中获取到label
+            fcmatches: List[AndroidFileContext] = self.get_file_context_matches(file)
+            # 没有匹配的文件context，或者文件是一个挂载点
+            if len(fcmatches) == 0 or file in self.init.asp.combined_fs.mount_points:
                 genfs_matches: List[Tuple[str, str, Context]] = []
+                # 遍历所有的挂载点，验证该文件是否是挂载的文件系统中的文件
                 for mount_path, mp in self.init.asp.combined_fs.mount_points.items():
-                    # 遍历所有的挂载点，验证该文件是否是挂载的文件系统中的文件
                     if file.startswith(mount_path):
                         relfs : str = file[len(mount_path):]
                         fstype: str = mp.type
                         if relfs == "": relfs = "/"
-                        if fstype in self.sepol.genfs:  # 
+                        if fstype in self.sepol.genfs:  # 如果是挂载的文件系统，那么就要找到对应的genfscon
                             for genfscon in self.sepol.genfs[fstype]:
                                 if re.match(r'^' + genfscon.path + r'.*', relfs):
                                     genfs_matches += [(mount_path, genfscon.path, genfscon.context)]
                                 pass
-
+                        elif fstype in self.sepol.fs_use:
+                            if fstype != "tmpfs": continue  # 目前只处理tmpfs
+                            genfs_matches += [(mount_path, '/', self.sepol.fs_use[fstype].context)]
                             pass
                         pass
+                if len(genfs_matches) == 0:
+                    # 寄
+                    if self.init.asp.combined_fs.files[file].selinux is None:
+                        dropped_files.append(file)
+                        Logger.warn("No file context for %s" % file)
+                        continue
+                    pass
+                else:  # 生成了新的label
+                    genfs_matches = sorted(genfs_matches, reverse=True, key=lambda x: x[1])
+                    primary_path: str = genfs_matches[0][0]
+                    primary_match: SELinuxContext = SELinuxContext.FromString(genfs_matches[0][2])
+                    label_from_file_context = False
+                    pass
                 pass
-    
+            else:
+                ''' 在file_contexts文件中找到了匹配的文件context 找出最长的匹配
+                [AndroidFileContext<^/odm/etc/permissions(/.*)?$ -> u:object_r:odm_xml_file:s0>,
+                AndroidFileContext<^/(odm|vendor/odm)/etc(/.*)?$ -> u:object_r:vendor_configs_file:s0>,
+                AndroidFileContext<^/(odm|vendor/odm)(/.*)?$ -> u:object_r:vendor_file:s0>]
+                '''
+                max_prefix_len: int = 0
+                for afc in fcmatches:
+                    r = re.compile(r"\.|\^|\$|\?|\*|\+|\||\[|\(|\{")    # 一些通配符
+                    regex = afc.regex.pattern[1:len(afc.regex.pattern) - 1] # 去掉开头的^和结尾的$
+                    pos = r.search(regex)   # 找到第一个通配符的位置
+                    if pos:
+                        # 如果有通配符，那么就取通配符前面的字符串
+                        cur_prefix_len = pos.span()[0]
+                    else:
+                        cur_prefix_len = len(regex)
+                    if cur_prefix_len > max_prefix_len:
+                        max_prefix_len = cur_prefix_len
+                        primary_match = afc.context
+
+                pass
+            
+            # 如果原先没有context
+            if self.init.asp.combined_fs.files[file].selinux is None:
+                self.init.asp.combined_fs.files[file].selinux = primary_match
+                recovered_labels += 1
+            elif self.init.asp.combined_fs.files[file].selinux != primary_match:
+                if label_from_file_context: # file_context本身和文件系统的冲突
+                    Logger.warn("File context %s does not match file system context %s" % (primary_match, self.init.asp.combined_fs.files[file].selinux))
+                else:
+                    recovered_labels += 1
+                    self.init.asp.combined_fs.files[file].selinux = primary_match
+                pass
+
+            pass
+
+        for fn in dropped_files:
+            del self.init.asp.combined_fs.files[fn]
+            pass
+        if len(dropped_files) > 0:
+            Logger.warn("Dropped %d files with no file context" % len(dropped_files))
+            pass
+        Logger.info("Recovered %d file labels from file contexts" % recovered_labels)
+        
+
     def get_file_context_matches(self, filename: str) -> List[AndroidFileContext]:
         '''返回所有匹配的文件context，选最长的那个'''
         matches: List[AndroidFileContext] = []
